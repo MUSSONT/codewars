9d9561d8aaf78fbd18086d9b65f3061f54d74831
MUSSONT <37769039+MUSSONT@users.noreply.github.com>
2018-03-25 10:22:02 +0100

    soloution to first kata

---
+++ DescendingOrder.py
@@ -0,0 +1,5 @@
+def Descending_Order(num):
+    return int(''.join(str(e) for e in sorted([int(x) for x in str(num)], reverse=True)))
+
+#best answer def Descending_Order(num):
+#    return int("".join(sorted(str(num), reverse=True)))
